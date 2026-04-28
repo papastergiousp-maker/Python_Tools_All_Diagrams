66f51490367ec5f94d279cd15cbbfa6cf7d65614
papastergiousp <papastergiousp@gmail.com>
2026-04-28 17:13:47 +0300

    Delete Matplotib Senarios directory

--- Matplotib Senarios/Data.py
+++
@@ -1,7 +0,0 @@
-# Δεδομένα Premier League
-players = ["Harry Kane", "Mo Salah", "Bruno Fernandes", "Jamie Vardy", "Son Heung-min"]
-teams = ['Tottenham Hotspur', 'Liverpool', 'Manchester United', 'Leicester City', 'Tottenham Hotspur']
-goals = [23, 25, 21, 22, 19]  # Γκολ (σεζόν)
-assists = [8, 7, 11, 6, 10]   # Ασίστ (σεζόν)
-games = [34, 33, 34, 35, 33]  # Αγώνες που έπαιξαν (σεζόν)
-team_points = [65, 74, 70, 64, 65]  # Βαθμοί ομάδας (σεζόν)
